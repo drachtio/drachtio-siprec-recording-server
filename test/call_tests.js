04954341dba7d555f52ba7659425459da8d907d8
Dave Horton <daveh@beachdognet.com>
2021-01-15 09:08:37 -0500

    update deps

--- test/call_tests.js
+++ test/call_tests.js
@@ -62,38 +62,6 @@ test('siprec with rtpengine recorder', (t) => {
     });
 }) ;
 
-test('siprec with freeswitch recorder', (t) => {
-  t.timeoutAfter(20000);
-
-  clearRequire('..');
-  clearRequire('../lib/utils');
-  clearRequire('config');
-  process.env.NODE_CONFIG_ENV = 'test2';
-
-  const vmap = `-v ${__dirname}/scenarios:/tmp`;
-  const args = 'drachtio/sipp sipp -m 1 -sf /tmp/uac_siprec_pcap2.xml drachtio';
-  const cmd = `docker run -t --rm --net test_siprec ${vmap} ${args}`;
-
-  const srf = require('..');
-  srf
-    .on('connect', () => {
-
-      console.log(`cmd: ${cmd}`);
-      execCmd(cmd)
-        .then(() => {
-          t.pass('siprec with freeswitch passed');
-          srf.disconnect();
-          return t.end();
-        })
-        .catch((err) => {
-          t.end(err, 'test failed');
-        });
-    })
-    .on('error', (err) => {
-      t.end(err, 'error connecting to drachtio');
-    });
-}) ;
-
 test('siprec with multiple rtpengines', (t) => {
   t.timeoutAfter(20000);
 
@@ -125,4 +93,35 @@ test('siprec with multiple rtpengines', (t) => {
     .on('error', (err) => {
       t.end(err, 'error connecting to drachtio');
     });
+  }) ;
+  test('siprec with freeswitch recorder', (t) => {
+    t.timeoutAfter(20000);
+  
+    clearRequire('..');
+    clearRequire('../lib/utils');
+    clearRequire('config');
+    process.env.NODE_CONFIG_ENV = 'test2';
+  
+    const vmap = `-v ${__dirname}/scenarios:/tmp`;
+    const args = 'drachtio/sipp sipp -m 1 -sf /tmp/uac_siprec_pcap.xml drachtio';
+    const cmd = `docker run -t --rm --net test_siprec ${vmap} ${args}`;
+  
+    const srf = require('..');
+    srf
+      .on('connect', () => {
+  
+        console.log(`cmd: ${cmd}`);
+        execCmd(cmd)
+          .then(() => {
+            t.pass('siprec with freeswitch passed');
+            srf.disconnect();
+            return t.end();
+          })
+          .catch((err) => {
+            t.end(err, 'test failed');
+          });
+      })
+      .on('error', (err) => {
+        t.end(err, 'error connecting to drachtio');
+      });
 }) ;
