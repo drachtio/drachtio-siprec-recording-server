88c9464642a492ec9282499e306ff9fbd8b996a8
Quan HL <quan.luuhoang8@gmail.com>
2023-12-25 12:26:55 +0700

    fix rtpengine re-invite inactive sdp

--- test/call_tests.js
+++ test/call_tests.js
@@ -40,7 +40,7 @@ test('siprec with rtpengine recorder', (t) => {
 
   const vmap = `-v ${__dirname}/scenarios:/tmp`;
   const args = 'drachtio/sipp sipp -m 1 -sf /tmp/uac_siprec_pcap.xml drachtio';
-  const cmd = `docker run -t --rm --net test_siprec ${vmap} ${args}`;
+  const cmd = `docker run -t --rm --net test_siprec ${vmap} ${args} -trace_msg`;
 
   const srf = require('..');
   srf
@@ -60,68 +60,4 @@ test('siprec with rtpengine recorder', (t) => {
     .on('error', (err) => {
       t.end(err, 'error connecting to drachtio');
     });
-}) ;
-
-test('siprec with multiple rtpengines', (t) => {
-  t.timeoutAfter(20000);
-
-  clearRequire('..');
-  clearRequire('../lib/rtpengine-call-handler');
-  clearRequire('../lib/utils');
-  clearRequire('config');
-  process.env.NODE_CONFIG_ENV = 'test3';
-
-  const vmap = `-v ${__dirname}/scenarios:/tmp`;
-  const args = 'drachtio/sipp sipp -m 2 -sf /tmp/uac_siprec_pcap.xml drachtio';
-  const cmd = `docker run -t --rm --net test_siprec ${vmap} ${args}`;
-
-  const srf = require('..');
-  srf
-    .on('connect', () => {
-
-      console.log(`cmd: ${cmd}`);
-      execCmd(cmd)
-        .then(() => {
-          t.pass('successfully connected two calls');
-          srf.disconnect();
-          return t.end();
-        })
-        .catch((err) => {
-          t.end(err, 'test failed');
-        });
-    })
-    .on('error', (err) => {
-      t.end(err, 'error connecting to drachtio');
-    });
-  }) ;
-  test('siprec with freeswitch recorder', (t) => {
-    t.timeoutAfter(20000);
-  
-    clearRequire('..');
-    clearRequire('../lib/utils');
-    clearRequire('config');
-    process.env.NODE_CONFIG_ENV = 'test2';
-  
-    const vmap = `-v ${__dirname}/scenarios:/tmp`;
-    const args = 'drachtio/sipp sipp -m 1 -sf /tmp/uac_siprec_pcap.xml drachtio';
-    const cmd = `docker run -t --rm --net test_siprec ${vmap} ${args}`;
-  
-    const srf = require('..');
-    srf
-      .on('connect', () => {
-  
-        console.log(`cmd: ${cmd}`);
-        execCmd(cmd)
-          .then(() => {
-            t.pass('siprec with freeswitch passed');
-            srf.disconnect();
-            return t.end();
-          })
-          .catch((err) => {
-            t.end(err, 'test failed');
-          });
-      })
-      .on('error', (err) => {
-        t.end(err, 'error connecting to drachtio');
-      });
 }) ;
